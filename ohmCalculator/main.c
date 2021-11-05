d6e40fdaab1a3ec25e29926dfda121945ff3150e
Jander Teodoro <79011974+janderteodoro@users.noreply.github.com>
2021-11-05 13:28:21 -0300

    feature(calculaResistance)

--- ohmCalculator/main.c
+++ ohmCalculator/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+
+
 int main () {
 
     char choice;
+    float a, b;
+
 
     printf("CALCULADORA DO ELETRICISTA\n");
     printf("[1] Calcular Tensao\n");
@@ -14,20 +18,34 @@ int main () {
     scanf("%c", &choice);
 
     switch(choice) {
+
+        float res;
         
         case '1':
+
             system("cls");
             printf("CALCULAR TENSAO");
             break;
 
         case '2':
+
             system("cls");
             printf("CALCULAR CORRENTE");
             break;
 
         case '3':
+
             system("cls");
-            printf("CALCULAR RESISTENCIA");
+            printf("CALCULAR RESISTENCIA\n");
+            printf("\n");
+            printf("Insira o valor de Tensao (V): ");
+            scanf("%f", &a);
+            printf("Insira o valor da Corrente (A): ");
+            scanf("%f", &b);
+            res = a / b; 
+            printf("Resistencia: %f", res);
+
+
             break;
 
         default: 
@@ -39,3 +57,4 @@ int main () {
 
     return 0;
 }
+
